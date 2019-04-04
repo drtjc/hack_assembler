2317860fc2059bcf4faa3197f7b6a34a6c5328bb
Tom curnock <curnock@gmail.com>
2019-04-03 22:33:27 -0500

    add parser and coder

--- HackAssembler.py
+++ HackAssembler.py
@@ -1,54 +1,65 @@
 import sys
 import os
+from collections import UserDict
 from typing import Dict
 
 
 # symbol table #########################################################
-symbol_table = {}
-var_idx = 16
+class SymbolTable(UserDict):
 
-for i in range(15):
-    symbol_table['R' + str(i)] = i
+    def __init__(self):
+        super().__init__()
 
-symbol_table['SCREEN'] = 16384
-symbol_table['KBD'] = 24576
+        for i in range(15):
+            self['R' + str(i)] = i
 
-symbol_table['SP'] = 0
-symbol_table['LCL'] = 1
-symbol_table['ARG'] = 2
-symbol_table['THIS'] = 3
-symbol_table['THAT'] = 4
+        self['SCREEN'] = 16384
+        self['KBD'] = 24576
 
-def add_label(label: str, value: int):
-    symbol_table[label] = value
+        self['SP'] = 0
+        self['LCL'] = 1
+        self['ARG'] = 2
+        self['THIS'] = 3
+        self['THAT'] = 4
 
-def add_variable(var: str):
-    if var not in symbol_table:
-        symbol_table[var] = var_idx
-        var_idx += 1
+        self.var_idx = 16
 
+    def add_label(self, label: str, value: int):
+        self[label] = value
+
+    def add_variable(self, var: str):
+        if var not in self:
+            self[var] = self.var_idx
+            self.var_idx += 1
 
 ########################################################################
 
-from pprint import pprint
 
+# parser ###############################################################
 class Parser():
 
-    def __init__(self, symbol_table: Dict):
+    def __init__(self, symbol_table: SymbolTable):
         self.line = ""
-        self.line_no = 0
+        self.line_no = -1
         self.symbol_table = symbol_table
+        self._first_pass = True
 
-    def parse(self, line: str, first_pass: bool = True):
+    @property
+    def first_pass(self) -> bool:
+        return self._first_pass
 
-        if not first_pass:
-            self.line_no = 0
+    @first_pass.setter
+    def first_pass(self, fp: bool) -> None:
+        if fp != self._first_pass:
+            self.line_no = -1
+        self._first_pass = fp
+        
+    def parse(self, line: str):
 
         # remove newline
         # remove leading and trailing spaces
         # remove comments
         self.line = line.rstrip('\n').strip().split('\\')[0].rstrip()
-        pprint(self.line)
 
         if self.line and self.line[0] == "(" and self.line[-1] == ")":
             # label symbol (which is not a line of code)
@@ -56,48 +67,155 @@ class Parser():
             self.line = ""
 
             # add symbol to symbol table
-            if first_pass:
-                self.symbol_table[label] = self.line_no
+            if self.first_pass:
+                self.symbol_table.add_label(label, self.line_no + 1)
             
-            print("label symbol:" + self.line)
-
-        # if line is not then increment line_no
+        # if line is not empty then increment line_no
         if self.line:
             self.line_no += 1
-        else:
-            print("not code")
-            return
 
-        if not first_pass:
+        if not self.first_pass:
             if self.line[0] == "@":
                 # variable symbol or goto (a label symbol)
-                var = self.line[1:]
-            
-                if var not in self.symbol_table:
-                    symbol_table[var] = 16
+                var = self.line[1:]            
+                self.symbol_table.add_variable(var)
+                self.line = "@" + str(self.symbol_table[var])
+
+        return self.line
+########################################################################
 
 
-        ####
+# code #################################################################
+class Code():
 
-        #else: # else if garbage code then raise error
-        #    raise ValueError("garbage code")
+    A_SIZE = 15
 
-        #print(f'line_no = {self.line_no}')
-        # if valid line, do stuff, add to line_no
-        
-        # if label symbol
-        # add_symbol('test', 35)
+    dest_table = {}
+    dest_table[''] = '000'
+    dest_table['M'] = '001'
+    dest_table['D'] = '010'
+    dest_table['MD'] = '011'
+    dest_table['A'] = '100'
+    dest_table['AM'] = '101'
+    dest_table['AD'] = '110'
+    dest_table['AMD'] = '111'
+    
+    comp_table = {}
+    comp_table['0'] = ('0', '101010')
+    comp_table['1'] = ('0', '111111')
+    comp_table['-1'] = ('0', '111010')
+    comp_table['D'] = ('0', '001100')
+    comp_table['A'] = ('0', '110000')    
+    comp_table['!D'] = ('0', '001101')
+    comp_table['!A'] = ('0', '110001')
+    comp_table['-D'] = ('0', '001111')
+    comp_table['-A'] = ('0', '110011')
+    comp_table['D+1'] = ('0', '011111')
+    comp_table['A+1'] = ('0', '110111')
+    comp_table['D-1'] = ('0', '001110')
+    comp_table['A-1'] = ('0', '110010')
+    comp_table['D+A'] = ('0', '000010')
+    comp_table['D-A'] = ('0', '010011')
+    comp_table['A-D'] = ('0', '000111')
+    comp_table['D&A'] = ('0', '000000')
+    comp_table['D|A'] = ('0', '010101')
+    comp_table['M'] = ('1', '110000')
+    comp_table['!M'] = ('1', '110001')
+    comp_table['-M'] = ('1', '110011')
+    comp_table['M+1'] = ('1', '110111')
+    comp_table['M-1'] = ('1', '110010')
+    comp_table['D+M'] = ('1', '000010')
+    comp_table['D-M'] = ('1', '010011')
+    comp_table['M-D'] = ('1', '000111')
+    comp_table['D&M'] = ('1', '000000')
+    comp_table['D|M'] = ('1', '010101')
+
+    jump_table = {}
+    jump_table[''] = '000'
+    jump_table['JGT'] = '001'
+    jump_table['JEQ'] = '010'
+    jump_table['JGE'] = '011'
+    jump_table['JLT'] = '100'
+    jump_table['JNE'] = '101'
+    jump_table['JLE'] = '110'
+    jump_table['JMP'] = '111'
+ 
+    @classmethod
+    def dec_bin(cls, dec: int, digit: int) -> str:
+        if dec >= 0:
+            bin1 = bin(dec).split("0b")[1]
+            while len(bin1) < digit:
+                bin1 = '0' + bin1
+            return bin1
+        else:
+            bin1 = str(-1 * dec)
+            return bin(dec - pow(2, digit)).split("0b")[1]
+
+    def code(self, instruction: str) -> str:
+        if instruction[0] == "@": # A-instruction
+            return self._a_instruction(instruction)
+        else: # C-instruction
+            return self._c_instruction(instruction)
+
+    def _a_instruction(self, instruction: str) -> str:
+            A_10 = int(instruction[1:])
+            A_2 = self.dec_bin(A_10, self.A_SIZE)
+            b = '0' + A_2
+            return b
+
+    def _c_instruction(self, instruction: str) -> str:
+        dest = ""
+        comp = ""
+        jump = ""
+
+        s1 = instruction.split('=')
+        if len(s1) != 1: # there is a = sign
+            dest = s1[0].strip()
+            s2 = s1[1].strip()
+        else:
+            s2 = s1[0]
+
+        s3 = s2.split(';')
+        comp = s3[0].strip()
+        if len(s3) != 1: # there is a ; sign
+            jump = s3[1]
+  
+        print(f'd={dest}')
+        print(f'c={comp}')
+        print(f'j={jump}')
+
+        b = "111" # C-instruction always starts with 111 
+        b = b + self.comp_table[comp][0] + self.comp_table[comp][1]
+        b = b + self.dest_table[dest]
+        b = b + self.jump_table[jump] 
+
+        return b
+
+########################################################################
 
 
+c = Code()
+print(c.code("@9"))
+
+print(c.code("MD=D+1"))
+print(c.code("MD = D+1"))
+
+
+
+
+symbol_table = SymbolTable()
 p = Parser(symbol_table)
+
 p.parse("   abc   ")
 p.parse("   abc   \n")
-p.parse("   abc   \\ hi there\n")
+print(p.parse("   abc   \\ hi there\n"))
 p.parse("  \\ abc   ")
 p.parse("     \n")
-p.parse("  (STOP)  \\ \n")
-p.parse("@i", False)
+print(p.parse("  (STOP)  \\ \n"))
 
+p.first_pass = False
+print(p.parse("@i"))
+print(p.parse("@sum"))
 
 
 
